1a78f6a4b2731ce91059a502e0f55b805582cfb9
Chris Laganas <34030772+chrlag@users.noreply.github.com>
2020-08-23 11:04:31 +0300

    Update CountingValleys.java

--- Interview Preparation Kit/Warm-up Challenges/CountingValleys.java
+++ Interview Preparation Kit/Warm-up Challenges/CountingValleys.java
@@ -1,14 +1,9 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
 
 public class CountingValleys {
-
-    // Complete the countingValleys function below.
+    
     static int countingValleys(int n, String s) {
 
         int down = 0;
