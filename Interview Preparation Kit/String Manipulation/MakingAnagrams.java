7ce5c3c23ef90072e5f11af7cf707cb52829f52b
Chris Laganas <34030772+chrlag@users.noreply.github.com>
2020-08-23 11:36:30 +0300

    Create MakingAnagrams.java

---
+++ Interview Preparation Kit/String Manipulation/MakingAnagrams.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MakingAnagrams {
+
+    static int makeAnagram(String a, String b) {
+
+        int[] c = new int[26];
+        int[] d = new int[26];
+
+        for (int i = 0; i <a.length(); i++){
+            char letter = a.charAt(i);
+            c[letter - 'a']++;
+        }
+
+        for (int i = 0; i <b.length(); i++){
+            char letter = b.charAt(i);
+            d[letter - 'a']++;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++){
+            count += Math.abs(c[i] - d[i]);
+        }
+
+        return count;
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String a = scanner.nextLine();
+
+        String b = scanner.nextLine();
+
+        int res = makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
