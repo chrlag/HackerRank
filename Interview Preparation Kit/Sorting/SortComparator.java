611679f516eeb6e956ad10704aadcc15baa372c4
Chris Laganas <34030772+chrlag@users.noreply.github.com>
2020-08-23 11:35:35 +0300

    Create SortComparator.java

---
+++ Interview Preparation Kit/Sorting/SortComparator.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+class Checker implements Comparator<Player> {
+	public int compare(Player a, Player b) {
+
+        int x = 0;
+
+        if (a.score > b.score){
+            x = -1;
+        }
+        else if (a.score < b.score){
+            x = 1;
+        }
+        else{
+            x = a.name.compareTo(b.name);
+        }
+
+        return x;
+    
+    }
+}
+
+
+public class SortComparator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
